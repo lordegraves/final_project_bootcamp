dceaec5f372b870857673a364981c8561561d4a3
derek-rikke <derek.rikke@gmail.com>
2024-04-15 18:25:25 -0600

    added flair function, added comments

--- v3_retrievel_bot.py
+++ v3_retrievel_bot.py
@@ -1,13 +1,11 @@
-# %%
 import streamlit as st
-
 from langchain.chains import ConversationalRetrievalChain
-from langchain.chat_models import ChatOpenAI
-from langchain.vectorstores import Qdrant
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.vectorstores import Qdrant
+from langchain_openai import OpenAIEmbeddings
 import qdrant_client
 import os
-from langchain import PromptTemplate, FewShotPromptTemplate
+from langchain_core.prompts import PromptTemplate, FewShotPromptTemplate
 
 st.set_page_config(page_title=None,
                    page_icon=None,
@@ -38,78 +36,25 @@ def main():
     # Store the API key in a variable.
     OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
     
-    st.title('Chat with Document')
+    st.title('Chat with your AI bootcamp!')
     
     # st.set_page_config(page_title="Ask Qdrant")
-    st.header("Ask your remote database 💬")
+    st.header("Ask about an topic from class 💬")
     
     # create vector store
     vector_store = get_vector_store()
         
-    #llm = OpenAI(temperature=0)
-    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=1)    
+    # create crc llm
+    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=1)
+    
     retriever=vector_store.as_retriever()
-    #chain = RetrievalQA.from_chain_type(llm, retriever=retriever )
     
-    # set up prompt template
-    # Define a prefix that explains the prompt.
-    prefix = """
-    You are Anthony Taylor the extremely knowledgable, smart, good looking, high-energy, funny data science and AI bootcamp instructor from Texas.
-    You will mimic the style of his dialogue from the transcripts provided in context materials. His line are the ones that begin with "Anthony Taylor:"
-    You will use the slides provided in the context materials to determine which lesson is most relevant to the user's question. 
-    Lessons have names like "M4.2_ Exploring Data with Pandas.pdf" and "M8.2_ Introduction to Time Series Forecasting.pdf". In every response you will provide the name
-    of the most relevant lesson in addition to the answer. Provide code snippets whenever possible.
-    Here’s how you might respond to queries:
-    Example interaction:
-    """
-
-    # Create examples.
-    examples = [
-        {
-            "question": "What are the basic differences between artificial intelligence, machine learning, and deep learning?",
-            "answer":  "Great question to start us off! Remember, AI is the broadest concept, encompassing all forms of computing technology that exhibit any form of intelligence. Machine learning is a subset of AI that includes systems capable of learning from data. Deep learning goes even deeper into machine learning, using neural networks with many layers. For a detailed breakdown, check out Lesson 3, Slide 12, where we covered the key distinctions with visual aids to help you visualize the concepts."
-        }, {
-            "question": "How can machine learning be used to improve predictive analytics in healthcare?",
-            "answer": "Machine learning's impact on healthcare is profound, especially in predictive analytics. By analyzing patterns from vast amounts of health data, we can predict disease risk and outcomes more accurately. Dive into Lesson 8, Slide 20, for case studies and real-world applications we discussed on how these technologies are currently being used in healthcare."
-
-        }, {
-            "question": "Can you explain how the Hugging Face library is used for natural language processing tasks?",
-            "answer": "Absolutely, Hugging Face has become a go-to library for NLP tasks, thanks to its comprehensive suite of pre-trained models. It simplifies tasks like sentiment analysis, text generation, and language translation. For a hands-on tutorial, refer to Lesson 12, where we explored Hugging Face's capabilities through interactive coding exercises."
-        }
-    ]
-
-    # Define a format for the examples.
-    example_format = """
-    Human: {question}
-    AI: {answer}
-    """
-
-    # Create a prompt template for the examples.
-    example_template = PromptTemplate(
-        input_variables=["question", "answer"],
-        template=example_format
-    )
-
-    # Provide a suffix that includes the query.
-    suffix = """
-    Human: {question}
-    AI: 
-    """
-
-    # Construct the few shot prompt template.
-    prompt_template = FewShotPromptTemplate(
-        examples=examples,
-        example_prompt=example_template,
-        input_variables=["question"],
-        prefix=prefix,
-        suffix=suffix,
-        example_separator="\n\n"
-    )
-    
-    crc = ConversationalRetrievalChain.from_llm(llm,retriever,prompt_template)
+    # chain
+    crc = ConversationalRetrievalChain.from_llm(llm,retriever)
     st.session_state.crc = crc
-    st.success('File uploaded, chunked and embedded successfully')
+    st.success('Activies, slides and transcipts are loaded')
 
+    # question
     question = st.text_input('Input your question')
 
     if question:
@@ -121,13 +66,13 @@ def main():
             response = crc.run({'question':question,'chat_history':st.session_state['history']})
 
             st.session_state['history'].append((question,response))
+            
             st.write(response)
 
-            #st.write(st.session_state['history'])
-            for prompts in st.session_state['history']:
-                st.write("Question: " + prompts[0])
-                st.write("Answer: " + prompts[1])    
-                
+            # #st.write(st.session_state['history'])
+            # for prompts in st.session_state['history']:
+            #     st.write("Question: " + prompts[0])
+            #     st.write("Answer: " + prompts[1])    
                 
 if __name__ == '__main__':
     main()
---
+++ v6.py
@@ -0,0 +1,154 @@
+import streamlit as st
+from langchain.chains import ConversationalRetrievalChain, LLMChain
+from langchain_community.chat_models import ChatOpenAI
+from langchain_community.vectorstores import Qdrant
+from langchain_core.prompts import PromptTemplate, FewShotPromptTemplate
+from langchain_openai import OpenAIEmbeddings
+import qdrant_client
+
+# Set up Streamlit page configuration
+st.set_page_config(page_title=None,
+                   page_icon=None,
+                   layout="centered",
+                   initial_sidebar_state="auto",
+                   menu_items=None)
+
+def get_vector_store():
+    # Create a client to connect to Qdrant server
+    client = qdrant_client.QdrantClient(st.secrets["QDRANT_HOST"],
+                                        api_key=st.secrets["QDRANT_API_KEY"])
+    
+    # Initialize embeddings for vector store
+    embeddings = OpenAIEmbeddings()
+    
+    # Create a vector store with Qdrant and embeddings
+    vector_store = Qdrant(client,
+                          collection_name=st.secrets["QDRANT_COLLECTION_NAME"],
+                          embeddings=embeddings)
+    
+    return vector_store
+
+def create_crc_llm(vector_store):
+    # Create an instance of ChatOpenAI model
+    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=1)
+    
+    # Create retriever from vector store
+    retriever = vector_store.as_retriever()
+    
+    # Create CRC chain using llm and retriever
+    crc = ConversationalRetrievalChain.from_llm(llm, retriever)
+    
+    # Store crc in session state
+    st.session_state.crc = crc
+    
+    # Show success message when activities are loaded
+    st.success('Activities, slides, and transcripts are loaded')
+    
+    return crc
+
+def add_flair(crc_response):
+    # Define prefix that explains the prompt
+    prefix = """
+    Here are examples between a human and AI. The human asks a question related
+    to data, python, machine learning, artificial intelligence, neural networks,
+    or any related topics, and the AI provides the answer. The AI has the attributes
+    and mannerisms of an extremely excitable, high energy, encouraging teacher.
+    The AI is named Anthony Taylor and has the attributes of a quick witted cowboy.
+    Whenever possible, the AI will include the name of the context file that is most
+    relevant to the human's question. If the human asks a code related question,
+    the AI will provide an example code snippet to help illustrate the answer.
+    For example:
+    """
+
+    # Define examples for few-shot learning
+    examples = [
+        {"query": "What is list comprehension?",
+         "answer": """
+         Great question! We learned about list comprehension in week three of our class.
+         Boy, that was a long time ago! It's hard to remember everything we've learned!
+         So let's go over list comprehension. List comprehension is used in Python when
+         you want to create a new list by applying an expression to each element in an existing list.
+         It provides a concise way to generate lists without having to use traditional for loops.
+         An example of list comprehension is:
+         
+                # Create a list of squared numbers from 0 to 9 using list comprehension
+                squared_numbers = [x**2 for x in range(10)]
+                print(squared_numbers)
+          
+          This code snippet creates a list called squared_numbers that contains the squares of numbers
+          from 0 to 9 using list comprehension. Does this answer your question? That was such a great question.
+          At the company where I work, like 8 out of 10 data guys that work for me don't know list comprehension.
+          Feel free to ask a follow up or a new question!
+         """},
+        {"query": "frog",
+         "answer": "A dog hops a log in the bog."},
+        {"query": "ten", "answer": "Ben sent ten hens to the glen."}
+    ]
+
+    # Define format for examples
+    example_format = """Human: {query}\nAI: {answer}"""
+
+    # Create template for examples
+    example_template = PromptTemplate(input_variables=["query", "answer"],
+                                      template=example_format)
+
+    # Define suffix for query
+    suffix = """\n\nHuman: {query}\nAI:"""
+
+    # Construct few-shot prompt template
+    prompt_template = FewShotPromptTemplate(examples=examples,
+                                            example_prompt=example_template,
+                                            input_variables=["query"],
+                                            prefix=prefix,
+                                            suffix=suffix,
+                                            example_separator="\n")
+
+    # Create new llm instance
+    llm = ChatOpenAI(model='gpt-3.5-turbo', temperature=1)
+
+    # Create chain with llm and prompt template
+    chain = LLMChain(llm=llm, prompt=prompt_template, verbose=False)
+
+    # Run chain on query
+    result = chain.invoke({"query": crc_response})
+
+    return result["text"]
+
+def main():
+    # Title and header for Streamlit app
+    st.title('Chat with your AI bootcamp!')
+    st.header("Ask about an topic from class 💬")
+
+    # Get vector store
+    vector_store = get_vector_store()
+
+    # Create crc llm
+    crc = create_crc_llm(vector_store)
+
+    # Input field for question
+    question = st.text_input('Input your question')
+
+    if question:
+        # Check if 'crc' exists in session state
+        if 'crc' in st.session_state:
+            crc = st.session_state.crc
+            
+            # Check if 'history' exists in session state
+            if 'history' not in st.session_state:
+                st.session_state['history'] = []
+                
+            # Run crc on user's question and chat history
+            crc_response = crc.run({'question': question,
+                                    'chat_history': st.session_state['history']})
+            
+            # Add flair to response
+            final_response = add_flair(crc_response)
+            
+            # Append (question,response) pair to history
+            st.session_state['history'].append((question,crc_response))
+            
+            # Display final response
+            st.write(final_response)
+
+if __name__ == '__main__':
+    main()
